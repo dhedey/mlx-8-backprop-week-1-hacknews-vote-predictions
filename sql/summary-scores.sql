6b7e3d4c9dbc3905073f8c81b902586621905267
David Edey <github@david-edey.com>
2025-06-09 16:20:00 +0100

    Add summary scores

---
+++ sql/summary-scores.sql
@@ -0,0 +1,21 @@
+WITH
+data AS (
+	SELECT
+		score
+	FROM hacker_news.items
+	WHERE
+	    type = 'story'
+        AND title IS NOT NULL
+        AND url IS NOT NULL
+        AND score IS NOT NULL
+		AND dead IS NULL OR dead = false
+)
+SELECT
+	score,
+	COUNT(1) as count
+FROM data
+GROUP BY
+	score
+ORDER BY
+	score ASC
+;
