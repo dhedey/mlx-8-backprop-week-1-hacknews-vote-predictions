5c1ba71c1ddf487cf8888727bddb07c815d6ea17
David Edey <github@david-edey.com>
2025-06-09 16:57:04 +0100

    More summary statistics

---
+++ sql/summary-by-author.sql
@@ -0,0 +1,25 @@
+WITH data AS (
+	SELECT
+		by AS author,
+	    LOG(score) AS log_10_score,
+		score
+	FROM hacker_news.items_by_month
+	WHERE
+	    type = 'story'
+        AND title IS NOT NULL
+        AND url IS NOT NULL
+        AND score IS NOT NULL
+        AND score >= 1
+		AND (dead IS NULL OR dead = false)
+)
+SELECT
+	author,
+	COUNT(1) as total_count,
+	AVG(score) as avg_score,
+	AVG(log_10_score) AS avg_log_10_score
+FROM data
+GROUP BY
+	author
+ORDER BY
+	total_count DESC
+;
---
+++ sql/summary-scores-for-single-author.sql
@@ -0,0 +1,24 @@
+WITH
+data AS (
+	SELECT
+		score
+	FROM hacker_news.items
+	WHERE
+	    type = 'story'
+        AND title IS NOT NULL
+        AND url IS NOT NULL
+        AND score IS NOT NULL
+		AND (dead IS NULL OR dead = false)
+		-- AND by = 'rbanffy' -- Most frequent author
+		-- AND by = 'Tomte' -- 2nd most frequent author
+		AND by = 'bond' -- 1212nd most frequent author
+)
+SELECT
+	score,
+	COUNT(1) as count
+FROM data
+GROUP BY
+	score
+ORDER BY
+	score ASC
+;
