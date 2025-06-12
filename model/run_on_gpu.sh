0d8fc53df370b2ccc31d334de8569e3371cc3087
David Edey <github@david-edey.com>
2025-06-12 16:42:44 +0100

    tweak: Update run on gpu instructions

--- model/run_on_gpu.sh
+++ model/run_on_gpu.sh
@@ -5,10 +5,10 @@ apt-get install git
 apt-get install git-lfs
 git lfs install
 curl -LsSf https://astral.sh/uv/install.sh | sh
+source ~/.local/bin/env
 
 cd ~
 git clone https://github.com/dhedey/mlx-8-backprop-week-1-hacknews-vote-predictions.git
-
 cd mlx-8-backprop-week-1-hacknews-vote-predictions
-~/.local/bin/uv sync
-~/.local/bin/uv run ./model/train.py --batch-size 1024
+
+uv run ./model/train.py --batch-size 1024
